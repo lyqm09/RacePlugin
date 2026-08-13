bcbb0b0835d230d9e92fcbd1cb3e200f8a502833
Lyam <maes.lyam@gmail.com>
2026-08-13 17:30:55 +0200

    refactor: add RaceData

--- src/main/java/be/lymaes/race/data/IRaceData.java
+++ src/main/java/be/lymaes/race/data/IRaceData.java
@@ -1,4 +1,18 @@
 package be.lymaes.race.data;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public interface IRaceData {
+
+
+
+    int getRank();
+    void rankUp();
+
+    int getExp();
+    void addExp(int n);
+    void subExp(int n);
+
+    void updateProfileData(ObjectNode rootNode);
+
 }
--- src/main/java/be/lymaes/race/data/RaceData.java
+++ src/main/java/be/lymaes/race/data/RaceData.java
@@ -1,4 +1,70 @@
 package be.lymaes.race.data;
 
-public class RaceData {
+import be.lymaes.race.model.RaceType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public abstract class RaceData implements IRaceData {
+
+    private final RaceType race;
+    private final int subrace;
+
+    private int rank;
+    private int exp;
+
+    public RaceData(RaceType race, int subrace, int rank, int exp) {
+        this.race = race;
+        this.subrace = subrace;
+        this.rank = rank;
+        this.exp = exp;
+    }
+
+    public RaceType getRace() {
+        return race;
+    }
+
+    public int getSubrace() {
+        return subrace;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void rankUp() {
+        rank++;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void addExp(int n) {
+        exp += n;
+    }
+
+    public void subExp(int n) {
+        exp -= n;
+    }
+
+    // save and load
+
+    public final void updateProfileData(ObjectNode rootNode) {
+        ObjectNode raceNode = rootNode.withObjectProperty(race.name());
+
+        raceNode.put("subrace", subrace);
+        ObjectNode subraceNode = raceNode.withObjectProperty(Integer.toString(subrace));
+
+        subraceNode.put("exp", exp);
+        subraceNode.put("rank", rank);
+
+        writeSpecificData(subraceNode);
+    }
+
+    protected abstract void writeSpecificData(ObjectNode subraceNode);
+
+    public final void loadProfileData(JsonNode rootNode) {
+        
+    }
+
 }
