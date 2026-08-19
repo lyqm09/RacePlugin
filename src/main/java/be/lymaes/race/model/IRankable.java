22b3124b52f0823af4ff0c6c73200e79e21bd32b
Lyam <maes.lyam@gmail.com>
2026-08-19 12:42:03 +0200

    none: delete empty line

--- src/main/java/be/lymaes/race/model/IRankable.java
+++ src/main/java/be/lymaes/race/model/IRankable.java
@@ -4,7 +4,6 @@ import be.lymaes.race.RaceProfile;
 
 public interface IRankable {
 
-
     void addExp(RaceProfile profile, int n);
     String getRankName(int rank);
     int getExpRequired(int rank);
